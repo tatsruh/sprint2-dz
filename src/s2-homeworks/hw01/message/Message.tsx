fcf8de0e964ccd8a13cc5f8121e0626b1155e7f7
Tania <tania@ivans-mbp-2.home>
2024-10-16 21:53:00 +0200

    css

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -1,28 +1,39 @@
 import React from 'react'
 import s from './Message.module.css'
 
-// нужно создать правильный тип вместо any
-export type MessagePropsType = any
 
-// нужно отобразить приходящие данные
-const Message = (props: MessagePropsType) => {
+// нужно создать правильный тип вместо any
+export type MessagePropsType = {
+    message: {
+        id: number,
+        user: {
+            avatar: string,
+            name: string,
+        }
+        message: {
+            text: string,
+            time: string,
+        }
+    }
+    }
+
+
+
+
+ const Message = (props: MessagePropsType) => {
     return (
         <div id={'hw1-message-' + props.message.id} className={s.message}>
             <div className={s.imageAndText}>
-                <img
+                <img src={props.message.user.avatar}
+                     alt={'avatar'}
                     id={'hw1-avatar-' + props.message.id}
-                    // создаёт студент
-
-                    //
                 />
                 <div className={s.text}>
                     <div id={'hw1-name-' + props.message.id} className={s.name}>
-                        {/*создаёт студент*/}
-
-                        {/**/}
+                        {props.message.user.name}
                     </div>
                     <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
-                        {/*создаёт студент*/}
+                        {props.message.message.text}
 
                         {/**/}
                     </pre>
@@ -30,7 +41,7 @@ const Message = (props: MessagePropsType) => {
             </div>
             <div id={'hw1-time-' + props.message.id} className={s.time}>
                 {/*создаёт студент*/}
-
+                {props.message.message.time}
                 {/**/}
             </div>
         </div>
